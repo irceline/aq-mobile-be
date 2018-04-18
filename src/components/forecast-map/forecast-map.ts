b446ab996389d6a616bbd6c5c966d21bd380de17
opeeters <opeeters@gmail.com>
2018-04-18 09:52:31 +0200

    first try forecast map

--- src/components/forecast-map/forecast-map.ts
+++ src/components/forecast-map/forecast-map.ts
@@ -16,30 +16,19 @@ export class ForecastMapComponent implements AfterViewInit {
   }
 
   private createMap() {
-    this.map = L.map('forecast-map', {}).setView([51.505, -0.09], 13);
+    this.map = L.map('forecast-map', {}).setView([50.5, 4.5], 7);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
 
-    var marker = L.marker([51.5, -0.09]).addTo(this.map);
-
-    var circle = L.circle([51.508, -0.11], {
-      color: 'red',
-      fillColor: '#f03',
-      fillOpacity: 0.5,
-      radius: 500
+    L.tileLayer.wms("http://geo.irceline.be/forecast/wms", {
+		layers: 'o3_maxhmean',
+    transparent: true,
+    format: 'image/png',
+    opacity: 0.7
     }).addTo(this.map);
 
-    var polygon = L.polygon([
-      [51.509, -0.08],
-      [51.503, -0.06],
-      [51.51, -0.047]
-    ]).addTo(this.map);
-
-    marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-    circle.bindPopup("I am a circle.");
-    polygon.bindPopup("I am a polygon.");
 
   }
 
