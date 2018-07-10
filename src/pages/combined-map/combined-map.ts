1c5a9e2401e8c66973c28277133c0d9d78016491
Jan Schulte <j.schulte@52north.org>
2018-07-10 12:03:26 +0200

    add layer to combined-map

--- src/pages/combined-map/combined-map.ts
+++ src/pages/combined-map/combined-map.ts
@@ -2,7 +2,8 @@ import { AfterViewInit, ChangeDetectorRef, Component } from '@angular/core';
 import { DatasetApiInterface, ParameterFilter, Phenomenon, Platform, SettingsService } from '@helgoland/core';
 import { GeoSearchOptions, LayerOptions, MapCache } from '@helgoland/map';
 import { ModalController, NavController, NavParams } from 'ionic-angular';
-import L from 'leaflet';
+import L, { WMSOptions } from 'leaflet';
+import moment from 'moment';
 
 import { ModalPhenomenonSelectorComponent } from '../../components/modal-phenomenon-selector/modal-phenomenon-selector';
 import { StationSelectorComponent } from '../../components/station-selector/station-selector';
@@ -10,22 +11,39 @@ import { IrcelineSettingsProvider } from '../../providers/irceline-settings/irce
 import { MobileSettings } from '../../providers/settings/settings';
 import { DiagramPage } from '../diagram/diagram';
 
+enum PhenomenonLabel {
+  NO2 = 'NO2',
+  O3 = 'O3',
+  PM10 = 'PM10',
+  PM25 = 'PM25',
+  BC = 'BC',
+  OTHERS = 'Others'
+}
+
+enum TimeLabel {
+  current = 'current',
+  today = 'today',
+  tomorrow = 'tomorrow',
+  today2 = 'today2',
+  today3 = 'today3'
+}
+
 const phenomenonMapping = [
   {
     id: '8',
-    label: 'NO2'
+    label: PhenomenonLabel.NO2
   }, {
     id: '7',
-    label: 'O3'
+    label: PhenomenonLabel.O3
   }, {
     id: '5',
-    label: 'PM10'
+    label: PhenomenonLabel.PM10
   }, {
     id: '6001',
-    label: 'PM25'
+    label: PhenomenonLabel.PM25
   }, {
     id: '391',
-    label: 'BC'
+    label: PhenomenonLabel.BC
   }
 ]
 
@@ -35,10 +53,9 @@ const phenomenonMapping = [
 })
 export class CombinedMapPage implements AfterViewInit {
 
-  public map: L.Map;
   public geoSearchOptions: GeoSearchOptions;
-  public phenomenonLabel: string = 'NO2';
-  public time: string = 'today';
+  public phenomenonLabel: PhenomenonLabel = PhenomenonLabel.NO2;
+  public time: TimeLabel = TimeLabel.current;
   public selectedOtherPhenom: string;
 
   public providerUrl: string;
@@ -48,7 +65,6 @@ export class CombinedMapPage implements AfterViewInit {
   public zoomControlOptions: L.Control.ZoomOptions = {};
   public overlayMaps: Map<string, LayerOptions> = new Map<string, LayerOptions>();
   public fitBounds: L.LatLngBoundsExpression;
-  public layerControlOptions: L.Control.LayersOptions = { position: 'bottomleft', hideSingleBase: true };
   public clusterStations: boolean;
   public selectedPhenomenon: Phenomenon;
 
@@ -81,19 +97,29 @@ export class CombinedMapPage implements AfterViewInit {
     const phenID = this.getPhenomenonID(this.phenomenonLabel);
     if (phenID) { this.getPhenomenonFromAPI(phenID) }
     this.selectedOtherPhenom = '';
+    if (this.phenomenonLabel == PhenomenonLabel.BC) {
+      this.time = TimeLabel.current;
+    }
+    this.showLayer();
   }
 
-  private getPhenomenonID(label: string): string {
+  public isDisabled(): boolean {
+    return this.phenomenonLabel == PhenomenonLabel.BC;
+  }
+
+  private getPhenomenonID(label: PhenomenonLabel): string {
     const phen = phenomenonMapping.find(e => label === e.label);
     if (phen) return phen.id;
   }
 
-  private getPhenomenonLabel(id: string): string {
+  private getPhenomenonLabel(id: string): PhenomenonLabel {
     const phen = phenomenonMapping.find(e => id === e.id);
     if (phen) return phen.label;
   }
 
-  public onTimeChange(): void { }
+  public onTimeChange(): void {
+    this.showLayer();
+  }
 
   public openOtherPhenomena() {
     const modal = this.modalCtrl.create(ModalPhenomenonSelectorComponent, {
@@ -135,9 +161,81 @@ export class CombinedMapPage implements AfterViewInit {
     this.selectedPhenomenon = selectedPhenomenon;
     this.phenomenonFilter = { phenomenon: selectedPhenomenon.id };
     this.phenomenonLabel = this.getPhenomenonLabel(selectedPhenomenon.id);
-    // this.ircelineSettings.getSettings(false).subscribe(settings =>
-    //   this.overlayMaps = this.layerGen.getLayersForPhenomenon(selectedPhenomenon.id, settings.lastupdate, true)
-    // );
+  }
+
+  private showLayer() {
+    this.overlayMaps = new Map<string, LayerOptions>();
+    let layerId: string;
+    let wmsUrl: string;
+    let timeParam: string;
+    if (this.time == TimeLabel.current) {
+      wmsUrl = 'http://geo.irceline.be/rioifdm/wms';
+      switch (this.phenomenonLabel) {
+        case PhenomenonLabel.BC:
+          layerId = 'bc_hmean';
+          break;
+        case PhenomenonLabel.NO2:
+          layerId = 'no2_hmean';
+          break;
+        case PhenomenonLabel.O3:
+          layerId = 'o3_hmean';
+          break;
+        case PhenomenonLabel.PM10:
+          layerId = 'pm10_hmean';
+          break;
+        case PhenomenonLabel.PM25:
+          layerId = 'pm25_hmean';
+          break;
+        default:
+          break;
+      }
+    } else {
+      wmsUrl = 'http://geo.irceline.be/forecast/wms';
+      switch (this.phenomenonLabel) {
+        case PhenomenonLabel.NO2:
+          layerId = 'no2_maxhmean';
+          break;
+        case PhenomenonLabel.O3:
+          layerId = 'o3_maxhmean';
+          break;
+        case PhenomenonLabel.PM10:
+          layerId = 'pm10_dmean';
+          break;
+        case PhenomenonLabel.PM25:
+          layerId = 'pm25_dmean';
+          break;
+        default:
+          break;
+      }
+      switch (this.time) {
+        case TimeLabel.today:
+          timeParam = moment().format('YYYY-MM-DD');
+          break;
+        case TimeLabel.tomorrow:
+          timeParam = moment().add(1, 'day').format('YYYY-MM-DD');
+          break;
+        case TimeLabel.today2:
+          timeParam = moment().add(2, 'day').format('YYYY-MM-DD');
+          break;
+        case TimeLabel.today3:
+          timeParam = moment().add(3, 'day').format('YYYY-MM-DD');
+          break;
+        default:
+          break;
+      }
+    }
+    const layerOptions: WMSOptions = {
+      layers: layerId,
+      transparent: true,
+      format: 'image/png',
+      opacity: 0.7
+    }
+    if (timeParam) { layerOptions.time = timeParam };
+    this.overlayMaps.set(layerId + wmsUrl + timeParam, {
+      label: layerId + wmsUrl + timeParam,
+      visible: true,
+      layer: L.tileLayer.wms(wmsUrl, layerOptions)
+    });
   }
 
 }
