15621acfef08701de560d6d249f83f259ec3b6ca
Jan Schulte <j.schulte@52north.org>
2018-08-27 09:04:59 +0200

    update tb version

--- src/components/station-selector/station-selector.ts
+++ src/components/station-selector/station-selector.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ColorService, Dataset, Platform, Station } from '@helgoland/core';
+import { ColorService, Dataset, Station } from '@helgoland/core';
 import { NavParams, ViewController } from 'ionic-angular';
 
 import { TimeseriesService } from '../../providers/timeseries/timeseries.service';
