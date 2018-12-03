a08f12c76dac47ea3dba434acf5ad149561d9470
Jan Schulte <j.schulte@52north.org>
2018-12-03 14:49:23 +0100

    add refresh gesture

--- src/pages/start/start.ts
+++ src/pages/start/start.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { IonAffixEvent } from 'ion-affix/dist/directives/ion-affix-event';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform, Refresher, ToastController } from 'ionic-angular';
 
 import { BelaqiSelection, HeaderContent } from '../../components/belaqi-user-location-slider/belaqi-user-location-slider';
+import { RefreshHandler } from '../../providers/refresh/refresh';
 import { MapPage } from '../map/map';
 
 @Component({
@@ -18,7 +19,10 @@ export class StartPage {
 
   constructor(
     private nav: NavController,
-    public translateSrvc: TranslateService
+    private refreshHandler: RefreshHandler,
+    private platform: Platform,
+    private toast: ToastController,
+    private translateSrvc: TranslateService
   ) { }
 
   public navigateToMap(selection: BelaqiSelection) {
@@ -38,6 +42,17 @@ export class StartPage {
     if (!event.sticky && this.sliderHeaderContent === null) {
       event.affix.headerElement.remove();
     }
+  }
+
+  public doRefresh(refresher: Refresher) {
+    this.refreshHandler.refresh();
+    if (this.platform.is('cordova')) {
+      this.toast.create({
+        message: this.translateSrvc.instant('refresh-button.message'),
+        duration: 3000
+      }).present();
+    }
+    setTimeout(() => refresher.complete(), 1000);
   }
 
 }
