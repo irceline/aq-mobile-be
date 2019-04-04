9ff7d7e92bb811ded0b5101a4af2ef3378523669
Jan Schulte <j.schulte@52north.org>
2019-04-04 17:18:25 +0200

    fix hook script for no android builds

--- hooks/before_compile.js
+++ hooks/before_compile.js
@@ -5,34 +5,53 @@ module.exports = function (context) {
 
     // remove add plugin in google-services file
     var servicesFile = 'platforms/android/cordova-support-google-services/aqmobile-build.gradle';
-    fs.readFile(servicesFile, 'utf-8', function (err, data) {
-        if (err) throw err;
-        data = data.replace('apply plugin: com.google.gms.googleservices.GoogleServicesPlugin', '');
-        fs.writeFile(servicesFile, data, 'utf-8', function (err) {
-            if (err) throw err;
+    fs.access(servicesFile, fs.constants.F_OK, (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        fs.readFile(servicesFile, 'utf-8', function (err, data) {
+            if (err) console.error(err);
+            data = data.replace('apply plugin: com.google.gms.googleservices.GoogleServicesPlugin', '');
+            fs.writeFile(servicesFile, data, 'utf-8', function (err) {
+                if (err) console.error(err);
+            });
         });
     });
 
     // adjust version in project properties
     var projectProperties = 'platforms/android/project.properties';
-    fs.readFile(projectProperties, 'utf-8', function (err, data) {
-        if (err) throw err;
-        data = data.replace('com.google.firebase:firebase-core:10.+', 'com.google.firebase:firebase-core:15.+');
-        data = data.replace('com.google.firebase:firebase-messaging:10.+', 'com.google.firebase:firebase-messaging:15.+');
-        data = data.replace('com.google.firebase:firebase-core:15.0.+', 'com.google.firebase:firebase-core:15.+');
-        fs.writeFile(projectProperties, data, 'utf-8', function (err) {
-            if (err) throw err;
+    fs.access(projectProperties, fs.constants.F_OK, (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        fs.readFile(projectProperties, 'utf-8', function (err, data) {
+            if (err) console.error(err);
+            data = data.replace('com.google.firebase:firebase-core:10.+', 'com.google.firebase:firebase-core:15.+');
+            data = data.replace('com.google.firebase:firebase-messaging:10.+', 'com.google.firebase:firebase-messaging:15.+');
+            data = data.replace('com.google.firebase:firebase-core:15.0.+', 'com.google.firebase:firebase-core:15.+');
+            fs.writeFile(projectProperties, data, 'utf-8', function (err) {
+                if (err) console.error(err);
+            });
         });
     });
 
     // adjust version in project properties
     var fcmPlugin = 'platforms/android/cordova-plugin-fcm-with-dependecy-updated/aqmobile-FCMPlugin.gradle';
-    fs.readFile(fcmPlugin, 'utf-8', function (err, data) {
-        if (err) throw err;
-        data = data.replace('com.google.firebase:firebase-core:10.+', 'com.google.firebase:firebase-core:15.+');
-        fs.writeFile(fcmPlugin, data, 'utf-8', function (err) {
-            if (err) throw err;
+    fs.access(fcmPlugin, fs.constants.F_OK, (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        fs.readFile(fcmPlugin, 'utf-8', function (err, data) {
+            if (err) console.error(err);
+            data = data.replace('com.google.firebase:firebase-core:10.+', 'com.google.firebase:firebase-core:15.+');
+            fs.writeFile(fcmPlugin, data, 'utf-8', function (err) {
+                if (err) console.error(err);
+            });
         });
     });
+
 
 };
