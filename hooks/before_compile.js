836773e47273d03c8e363ef456ab09db97341ce6
Jan Schulte <j.schulte@52north.org>
2019-04-09 09:47:37 +0200

    adjust build hooks

--- hooks/before_compile.js
+++ hooks/before_compile.js
@@ -3,22 +3,6 @@ var fs = require('fs');
 module.exports = function (context) {
     console.log('Adjustments before compile');
 
-    // remove add plugin in google-services file
-    var servicesFile = 'platforms/android/cordova-support-google-services/aqmobile-build.gradle';
-    fs.access(servicesFile, fs.constants.F_OK, (err) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        fs.readFile(servicesFile, 'utf-8', function (err, data) {
-            if (err) console.error(err);
-            data = data.replace('apply plugin: com.google.gms.googleservices.GoogleServicesPlugin', '');
-            fs.writeFile(servicesFile, data, 'utf-8', function (err) {
-                if (err) console.error(err);
-            });
-        });
-    });
-
     // adjust version in project properties
     var projectProperties = 'platforms/android/project.properties';
     fs.access(projectProperties, fs.constants.F_OK, (err) => {
@@ -36,22 +20,5 @@ module.exports = function (context) {
             });
         });
     });
-
-    // adjust version in project properties
-    var fcmPlugin = 'platforms/android/cordova-plugin-fcm-with-dependecy-updated/aqmobile-FCMPlugin.gradle';
-    fs.access(fcmPlugin, fs.constants.F_OK, (err) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        fs.readFile(fcmPlugin, 'utf-8', function (err, data) {
-            if (err) console.error(err);
-            data = data.replace('com.google.firebase:firebase-core:10.+', 'com.google.firebase:firebase-core:15.+');
-            fs.writeFile(fcmPlugin, data, 'utf-8', function (err) {
-                if (err) console.error(err);
-            });
-        });
-    });
-
 
 };
