9abe0fcef812a1ef098aa7c808540de2c29d57ae
speckij <j_spec05@uni-muenster.de>
2019-04-24 16:35:08 +0200

    fix grey border upon initally loading the map

--- src/app/components/belaqi-map-slider/belaqi-map-slider.component.ts
+++ src/app/components/belaqi-map-slider/belaqi-map-slider.component.ts
@@ -617,7 +617,7 @@ class MapView {
       const icondiv = divIcon({ className: 'marker', iconAnchor: point(10, 40) });
       const location = { lat: this.location.latitude, lng: this.location.longitude } as LatLngLiteral;
       const bounds = latLngBounds(location, location);
-      let boundsOptions: FitBoundsOptions = {maxZoom: 12 };
+      let boundsOptions: FitBoundsOptions = {paddingTopLeft: [0, 100], maxZoom: 12 };
       this.removePopups();
       if (selection) {
         if (selection.stationlocation) {
@@ -627,7 +627,7 @@ class MapView {
             .setContent(this.translateSrvc.instant('map.nearest-station'));
           map.addLayer(this.nextStationPopup);
           bounds.extend(station);
-          boundsOptions = {maxZoom: 12 };
+          boundsOptions = {paddingTopLeft: [0, 100], maxZoom: 12 };
         }
       }
       this.userLocationMarker = marker(location, { draggable: false, icon: icondiv });
