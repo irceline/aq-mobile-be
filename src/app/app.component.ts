ef57e83f702f2af0bff5a6ba4939c0266e63046c
opeeters <opeeters@gmail.com>
2019-04-24 19:17:41 +0200

    fix hardware back button on Android with Ionic 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, IonRouterOutlet } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
 import { PushNotificationsService } from './services/push-notifications/push-notifications.service';
 
 @Component({
@@ -10,13 +11,32 @@ import { PushNotificationsService } from './services/push-notifications/push-not
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+
+  @ViewChildren(IonRouterOutlet) routerOutlets: QueryList<IonRouterOutlet>;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
+    public router: Router,
     private pushNotifications: PushNotificationsService
   ) {
     this.initializeApp();
+    this.backButtonEvent();
+  }
+
+  ngOnInit() { }
+
+  backButtonEvent() {
+    this.platform.backButton.subscribe(() => {
+        this.routerOutlets.forEach((outlet: IonRouterOutlet) => {
+            if (this.router.url === '/tabs/start') {
+                navigator['app'].exitApp();
+            } else {
+                window.history.back();
+            }
+        });
+    });
   }
 
   initializeApp() {
