c9a0bf127a1b059be1728572f28fb1dea1ff5205
opeeters <opeeters@gmail.com>
2019-04-26 13:59:29 +0200

    fix #158

--- src/app/components/belaqi-map-slider/belaqi-map-slider.component.ts
+++ src/app/components/belaqi-map-slider/belaqi-map-slider.component.ts
@@ -618,7 +618,7 @@ class MapView {
       const icondiv = divIcon({ className: 'marker', iconAnchor: point(10, 40) });
       const location = { lat: this.location.latitude, lng: this.location.longitude } as LatLngLiteral;
       const bounds = latLngBounds(location, location);
-      let boundsOptions: FitBoundsOptions = {paddingTopLeft: [0, 100], maxZoom: 12 };
+      let boundsOptions: FitBoundsOptions = {paddingTopLeft: [0, 0], maxZoom: 12 };
       this.removePopups();
       if (selection) {
         if (selection.stationlocation) {
@@ -628,7 +628,7 @@ class MapView {
             .setContent(this.translateSrvc.instant('map.nearest-station'));
           map.addLayer(this.nextStationPopup);
           bounds.extend(station);
-          boundsOptions = {paddingTopLeft: [0, 100], maxZoom: 12 };
+          boundsOptions = {paddingTopLeft: [0, 0], maxZoom: 12 };
         }
       }
       this.userLocationMarker = marker(location, { draggable: false, icon: icondiv });
