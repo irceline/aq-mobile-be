8d7d464108a7552f25ddd7d4061e8e41aa7b37bf
speckij <j_spec05@uni-muenster.de>
2019-05-21 13:25:51 +0200

    adjust compilation hook adjusting firebase versions to new developments.

--- hooks/before_compile.js
+++ hooks/before_compile.js
@@ -12,9 +12,10 @@ module.exports = function (context) {
         }
         fs.readFile(projectProperties, 'utf-8', function (err, data) {
             if (err) console.error(err);
-            data = data.replace('com.google.firebase:firebase-core:10.+', 'com.google.firebase:firebase-core:15.+');
-            data = data.replace('com.google.firebase:firebase-messaging:10.+', 'com.google.firebase:firebase-messaging:15.+');
-            data = data.replace('com.google.firebase:firebase-core:15.0.+', 'com.google.firebase:firebase-core:15.+');
+            data = data.replace('com.google.firebase:firebase-core:+', 'com.google.firebase:firebase-core:16.0.8+');
+            data = data.replace('com.google.firebase:firebase-messaging:+', 'com.google.firebase:firebase-messaging:17.5.0+');
+            data = data.replace('com.google.firebase:firebase-config:+', 'com.google.firebase:firebase-config:16.4.1+');
+            data = data.replace('com.google.firebase:firebase-perf:+', 'com.google.firebase:firebase-perf:16.2.4+');
             fs.writeFile(projectProperties, data, 'utf-8', function (err) {
                 if (err) console.error(err);
             });
