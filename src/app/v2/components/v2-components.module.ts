98cb82586b50c44b7d623fcf65153e317153f0a4
Krešo Galić <kreso.galic8@gmail.com>
2020-05-04 18:07:42 +0200

    feat: onboarding screen

--- src/app/v2/components/v2-components.module.ts
+++ src/app/v2/components/v2-components.module.ts
@@ -1,23 +1,17 @@
-import {NgModule} from '@angular/core';
-import {AutoCompleteModule} from 'ionic4-auto-complete';
-import {CommonModule} from '@angular/common';
-import {FormsModule} from '@angular/forms';
-import {IonicModule} from '@ionic/angular';
-import {LocationPickerComponent} from './location-picker/location-picker.component';
+import { NgModule } from '@angular/core';
+import { AutoCompleteModule } from 'ionic4-auto-complete';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { LocationPickerComponent } from './location-picker/location-picker.component';
+import { OnboardingSliderComponent } from './onboarding-slider/onboarding-slider.component';
 
-const COMPONENTS = [
-    LocationPickerComponent
-]
+const COMPONENTS = [LocationPickerComponent, OnboardingSliderComponent];
 
 @NgModule({
-    imports: [
-        AutoCompleteModule,
-        CommonModule,
-        FormsModule,
-        IonicModule,
-    ],
+    imports: [AutoCompleteModule, CommonModule, FormsModule, IonicModule],
     declarations: COMPONENTS,
     exports: COMPONENTS,
-    entryComponents: COMPONENTS
+    entryComponents: COMPONENTS,
 })
-export class V2ComponentsModule { }
+export class V2ComponentsModule {}
