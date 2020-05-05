b74aa9e8fa0a7a7786355a68bcd4e8c4fbd91399
Krešo Galić <kreso.galic8@gmail.com>
2020-05-05 17:45:39 +0200

    fix: menu fixes, location swipe added

--- src/app/v2/components/location-swipe/location-swipe.component.ts
+++ src/app/v2/components/location-swipe/location-swipe.component.ts
@@ -1,14 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    Component,
+    OnInit,
+    ViewChild,
+    EventEmitter,
+    Output,
+} from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
-  selector: 'app-location-swipe',
-  templateUrl: './location-swipe.component.html',
-  styleUrls: ['./location-swipe.component.scss'],
+    selector: 'app-location-swipe',
+    templateUrl: './location-swipe.component.html',
+    styleUrls: ['./location-swipe.component.scss'],
 })
 export class LocationSwipeComponent implements OnInit {
+    @ViewChild(IonSlides) slides: IonSlides;
+    @Output() locationChange = new EventEmitter();
 
-  constructor() { }
+    sliderOptions = {
+        spaceBetween: 100,
+        centeredSlides: true,
+        slidesPerView: 3,
+    };
 
-  ngOnInit() {}
+    locations = ['Cepin', 'Osijek', 'Zagreb', 'Split', 'Rijeka'];
 
+    constructor() {}
+
+    ngOnInit() {}
+
+    // Emit location change
+    async slideChange() {
+        const index = await this.slides.getActiveIndex();
+        const location = this.locations[index];
+        this.locationChange.next(location);
+    }
 }
