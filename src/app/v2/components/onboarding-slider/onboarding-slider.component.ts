fd3cb0298841e412e8d8641ae798f54608e5ef14
Krešo Galić <kreso.galic8@gmail.com>
2020-05-06 09:48:15 +0200

    fix: onboarding refactor

--- src/app/v2/components/onboarding-slider/onboarding-slider.component.ts
+++ src/app/v2/components/onboarding-slider/onboarding-slider.component.ts
@@ -1,93 +1,14 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {IonSlides, NavController} from '@ionic/angular';
-import {animate, keyframes, state, style, transition, trigger, } from '@angular/animations';
-
-import {NotificationType, UserNotificationSetting} from '../user-notification-settings/user-notification-settings.component';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
+import { IonSlides, NavController } from '@ionic/angular';
 
 @Component({
     selector: 'app-onboarding-slider',
     templateUrl: './onboarding-slider.component.html',
     styleUrls: ['./onboarding-slider.component.scss'],
-    animations: [
-        trigger('bounce', [
-            state(
-                '*',
-                style({
-                    transform: 'translateX(0)',
-                })
-            ),
-            transition(
-                '* => rightSwipe',
-                animate(
-                    '700ms ease-out',
-                    keyframes([
-                        style({
-                            transform: 'translateX(0)',
-                            offset: 0,
-                        }),
-                        style({
-                            transform: 'translateX(-65px)',
-                            offset: 0.3,
-                        }),
-                        style({
-                            transform: 'translateX(0)',
-                            offset: 1.0,
-                        }),
-                    ])
-                )
-            ),
-            transition(
-                '* => leftSwipe',
-                animate(
-                    '700ms ease-out',
-                    keyframes([
-                        style({
-                            transform: 'translateX(0)',
-                            offset: 0,
-                        }),
-                        style({
-                            transform: 'translateX(65px)',
-                            offset: 0.3,
-                        }),
-                        style({
-                            transform: 'translateX(0)',
-                            offset: 1.0,
-                        }),
-                    ])
-                )
-            ),
-        ]),
-    ],
 })
 export class OnboardingSliderComponent implements OnInit {
     @ViewChild(IonSlides) slides: IonSlides;
-
-    btnText = 'Ga verder';
-    state = 'x';
-    language = 'e';
-
-    userSettings: UserNotificationSetting[] = [
-        {
-            notificationType: NotificationType.highConcentration,
-            enabled: true
-        },
-        {
-            notificationType: NotificationType.transport,
-            enabled: true
-        },
-        {
-            notificationType: NotificationType.activity,
-            enabled: false
-        },
-        {
-            notificationType: NotificationType.allergies,
-            enabled: true
-        },
-        {
-            notificationType: NotificationType.exercise,
-            enabled: false
-        }
-    ];
+    @Input() btnText: string;
 
     constructor(public navCtrl: NavController) {}
 
@@ -108,23 +29,8 @@ export class OnboardingSliderComponent implements OnInit {
         const isEnd = await this.slides.isEnd();
         if (isEnd) {
             this.btnText = 'Start de app';
-        }
-    }
-
-    // Getting active and prev index for animations
-    async slideMoved() {
-        const activeIndex = await this.slides.getActiveIndex();
-        const previousIndex = (await this.slides.getPreviousIndex()) || 0;
-
-        if (activeIndex >= previousIndex) {
-            this.state = 'rightSwipe';
         } else {
-            this.state = 'leftSwipe';
+            this.btnText = 'Ga verder';
         }
-    }
-
-    // Returning the state to x, so we can repeat animation
-    animationDone() {
-        this.state = 'x';
     }
 }
--- src/app/v2/screens/onboarding-screen/onboarding-screen.component.ts
+++ src/app/v2/screens/onboarding-screen/onboarding-screen.component.ts
@@ -1,14 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import {
+    UserNotificationSetting,
+    NotificationType,
+} from '../../components/user-notification-settings/user-notification-settings.component';
 
 @Component({
-  selector: 'app-intro-screen',
-  templateUrl: './onboarding-screen.component.html',
-  styleUrls: ['./onboarding-screen.component.scss'],
+    selector: 'app-intro-screen',
+    templateUrl: './onboarding-screen.component.html',
+    styleUrls: ['./onboarding-screen.component.scss'],
 })
 export class OnboardingScreenComponent implements OnInit {
+    language = 'e';
+    btnText = 'Ga verder';
 
-  constructor() { }
+    userSettings: UserNotificationSetting[] = [
+        {
+            notificationType: NotificationType.highConcentration,
+            enabled: true,
+        },
+        {
+            notificationType: NotificationType.transport,
+            enabled: true,
+        },
+        {
+            notificationType: NotificationType.activity,
+            enabled: false,
+        },
+        {
+            notificationType: NotificationType.allergies,
+            enabled: true,
+        },
+        {
+            notificationType: NotificationType.exercise,
+            enabled: false,
+        },
+    ];
 
-  ngOnInit() {}
+    constructor() {}
 
+    ngOnInit() {}
 }
