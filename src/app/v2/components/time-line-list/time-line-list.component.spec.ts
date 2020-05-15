addfd411d9a140847a1f55bb77cf7bcb22cd4d24
Antonio Copic <acopic.ext@autoscout24.com>
2020-05-15 17:10:49 +0200

    TimeLineListComponnet functional specs

--- src/app/v2/components/time-line-list/time-line-list.component.spec.ts
+++ src/app/v2/components/time-line-list/time-line-list.component.spec.ts
@@ -2,14 +2,18 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TimeLineListComponent } from './time-line-list.component';
+import { IonSlides } from '@ionic/angular';
+import { localStorageMock } from '../../testing/localStorage.mock';
 
-xdescribe('TimeLineListComponent', () => {
+fdescribe('TimeLineListComponent', () => {
   let component: TimeLineListComponent;
   let fixture: ComponentFixture<TimeLineListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TimeLineListComponent ],
+      declarations: [
+          TimeLineListComponent,
+          IonSlides],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
     .compileComponents();
@@ -18,10 +22,21 @@ xdescribe('TimeLineListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TimeLineListComponent);
     component = fixture.componentInstance;
+    component.slides = TestBed.createComponent(IonSlides).componentInstance;
+    component.items = localStorageMock.getIndexScores(5, 5);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit proper events on slide change', () => {
+    spyOn(component.slides, 'getActiveIndex').and.callFake(() => Promise.resolve(2));
+    spyOn(component.dayChange, 'next');
+    component.slideChange().then(() => {
+      expect(component.slides.getActiveIndex).toHaveBeenCalled();
+      expect(component.dayChange.next).toHaveBeenCalled();
+    });
   });
 });
