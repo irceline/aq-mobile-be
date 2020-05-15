ab94c911bc5d69ca8cfc98caa215e8bac2fcbd6b
Antonio Copic <acopic.ext@autoscout24.com>
2020-05-15 15:43:30 +0200

    LocationSwipeComponent functional test

--- src/app/v2/components/location-swipe/location-swipe.component.spec.ts
+++ src/app/v2/components/location-swipe/location-swipe.component.spec.ts
@@ -2,14 +2,19 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LocationSwipeComponent } from './location-swipe.component';
+import { IonSlides } from '@ionic/angular';
+import { localStorageMock } from '../../testing/localStorage.mock';
 
-xdescribe('LocationSwipeComponent', () => {
+describe('LocationSwipeComponent', () => {
   let component: LocationSwipeComponent;
   let fixture: ComponentFixture<LocationSwipeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LocationSwipeComponent ],
+      declarations: [
+          LocationSwipeComponent,
+          IonSlides
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
     .compileComponents();
@@ -18,10 +23,21 @@ xdescribe('LocationSwipeComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LocationSwipeComponent);
     component = fixture.componentInstance;
+    component.slides = TestBed.createComponent(IonSlides).componentInstance;
+    component.locations = JSON.parse(localStorageMock.getItem('belAir.userLocations'));
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit proper events on slide change', () => {
+    spyOn(component.slides, 'getActiveIndex').and.callFake(() => Promise.resolve(2));
+    spyOn(component.locationChange, 'next');
+    component.slideChange().then(() => {
+      expect(component.slides.getActiveIndex).toHaveBeenCalled();
+      expect(component.locationChange.next).toHaveBeenCalledWith(component.locations[2]);
+    });
   });
 });
