faae37e5a64eafc11b78556ac0aa118dc68787d3
Joshua Hulpiau <joshua@nebulae.be>
2020-05-19 12:13:00 +0200

    invert belaqi index, low = good , high = bad

--- src/app/v2/components/background/background.component.ts
+++ src/app/v2/components/background/background.component.ts
@@ -21,25 +21,25 @@ export class BackgroundComponent implements OnInit {
 
     private static getBackgroundForIndex(index: number) {
         switch (index) {
-            case 1:
-                return '/assets/images/backgrounds/1.png';
-            case 2:
-                return '/assets/images/backgrounds/2.png';
-            case 3:
-                return '/assets/images/backgrounds/3.png';
-            case 4:
-                return '/assets/images/backgrounds/4.png';
-            case 5:
-                return '/assets/images/backgrounds/5.png';
-            case 6:
-                return '/assets/images/backgrounds/6.png';
-            case 7:
-                return '/assets/images/backgrounds/7.png';
-            case 8:
-                return '/assets/images/backgrounds/8.png';
-            case 9:
-                return '/assets/images/backgrounds/9.png';
             case 10:
+                return '/assets/images/backgrounds/1.png';
+            case 9:
+                return '/assets/images/backgrounds/2.png';
+            case 8:
+                return '/assets/images/backgrounds/3.png';
+            case 7:
+                return '/assets/images/backgrounds/4.png';
+            case 6:
+                return '/assets/images/backgrounds/5.png';
+            case 5:
+                return '/assets/images/backgrounds/6.png';
+            case 4:
+                return '/assets/images/backgrounds/7.png';
+            case 3:
+                return '/assets/images/backgrounds/8.png';
+            case 2:
+                return '/assets/images/backgrounds/9.png';
+            case 1:
                 return '/assets/images/backgrounds/10.png';
             default:
                 return null;
