99554e382c5a1473c0e2a82bc35d8fbf1e004a0f
Antonio Copic <acopic.ext@autoscout24.com>
2020-05-19 13:42:08 +0200

    CircleChartComponent functional tests

--- src/app/v2/components/circle-chart/circle-chart.component.spec.ts
+++ src/app/v2/components/circle-chart/circle-chart.component.spec.ts
@@ -2,17 +2,24 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CircleChartComponent } from './circle-chart.component';
+import {BelAQIService} from '../../services/bel-aqi.service';
+import {TranslateTestingModule} from '../../testing/TranslateTestingModule';
+import {localStorageMock} from '../../testing/localStorage.mock';
 
-xdescribe('CircleChartComponent', () => {
+describe('CircleChartComponent', () => {
   let component: CircleChartComponent;
   let fixture: ComponentFixture<CircleChartComponent>;
+  let belAQIService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ CircleChartComponent ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [TranslateTestingModule]
     })
     .compileComponents();
+    belAQIService = TestBed.get(BelAQIService);
+    spyOn(belAQIService, 'getLabelForIndex').and.callThrough();
   }));
 
   beforeEach(() => {
@@ -23,5 +30,32 @@ xdescribe('CircleChartComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize data properly', () => {
+    let belAqiIndex;
+    belAQIService.$activeIndex.subscribe( ( newIndex ) => {
+      belAqiIndex = newIndex.indexScore;
+    });
+    const belAqiText = belAQIService.getLabelForIndex(belAqiIndex);
+
+    expect(component.belAqi).toEqual(belAqiIndex);
+    expect(belAQIService.getLabelForIndex).toHaveBeenCalledWith(belAqiIndex);
+    expect(component.title).toEqual(belAqiText);
+    expect(component.text).toContain(belAqiText);
+  });
+
+  it('should update when $activeIndex changes', () => {
+    const currentIndexScores = localStorageMock.getIndexScores(5, 5);
+    const currentIndex = currentIndexScores[3];
+    belAQIService.$activeIndex.next(currentIndex);
+    expect(component.belAqi).toEqual(currentIndex.indexScore);
+
+    const belAqiText = belAQIService.getLabelForIndex(currentIndex.indexScore);
+
+    expect(component.belAqi).toEqual(currentIndex.indexScore);
+    expect(belAQIService.getLabelForIndex).toHaveBeenCalledWith(currentIndex.indexScore);
+    expect(component.title).toEqual(belAqiText);
+    expect(component.text).toContain(belAqiText);
   });
 });
