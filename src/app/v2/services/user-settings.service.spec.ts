d75cc5652b2b939ae3c08c74ec885358c550d3bb
Antonio Copic <acopic.ext@autoscout24.com>
2020-05-19 14:17:21 +0200

    user-settings.service updating with new specs

--- src/app/v2/services/user-settings.service.spec.ts
+++ src/app/v2/services/user-settings.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { localStorageMock } from '../testing/localStorage.mock';
 import { UserSettingsService } from './user-settings.service';
 import { specHelper } from '../testing/spec-helper';
+import {UserLocation} from '../Interfaces';
 
 describe('UserLocationsService', () => {
   let service: UserSettingsService;
@@ -20,5 +21,20 @@ describe('UserLocationsService', () => {
     const userSettings = localStorageMock.getItem('belAir.userNotificationSettings');
     expect(service.getUserSavedLocations()).toEqual(JSON.parse(userLocations));
     expect(service.getUserNotificationSettings()).toEqual(JSON.parse(userSettings));
+  });
+
+  it('should add new location', () => {
+    const newLocation: UserLocation = {label: 'Antwerpen', type: 'user', id: 100};
+    service.addUserLocation(newLocation);
+    expect(service.getUserSavedLocations()).toContain(newLocation);
+  });
+
+  it('should remove a location', () => {
+    const userLocations: UserLocation[] = JSON.parse(localStorageMock.getItem('belAir.userLocations'));
+    const savedLocationsBefore = service.getUserSavedLocations();
+    service.removeUserLocation(userLocations[2]);
+    expect(service.getUserSavedLocations().length).toEqual(savedLocationsBefore.length - 1);
+    const savedLocationsAfter = savedLocationsBefore.filter(x => x.id !== userLocations[2].id);
+    expect(service.getUserSavedLocations()).toEqual(savedLocationsAfter);
   });
 });
