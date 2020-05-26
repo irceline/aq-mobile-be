1dbfecbfcbcdff0615f4dff1599420854d84a2fe
Antonio Copic <acopic.ext@autoscout24.com>
2020-05-26 13:54:16 +0200

    Added constants file

---
+++ src/app/v2/common/constants.ts
@@ -0,0 +1,12 @@
+export const backgroundImages = {
+    10: '/assets/images/backgrounds/1.png',
+    9: '/assets/images/backgrounds/2.png',
+    8: '/assets/images/backgrounds/3.png',
+    7: '/assets/images/backgrounds/4.png',
+    6: '/assets/images/backgrounds/5.png',
+    5: '/assets/images/backgrounds/6.png',
+    4: '/assets/images/backgrounds/7.png',
+    3: '/assets/images/backgrounds/8.png',
+    2: '/assets/images/backgrounds/9.png',
+    1: '/assets/images/backgrounds/10.png'
+};
