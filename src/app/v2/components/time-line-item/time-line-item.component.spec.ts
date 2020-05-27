99310150905cb5da7aee79045d27c4acc0ab22fa
Antonio Copic <acopic.ext@autoscout24.com>
2020-05-27 14:58:14 +0200

    TimeLineItemComponent refactoring and specs

--- src/app/v2/components/time-line-item/time-line-item.component.spec.ts
+++ src/app/v2/components/time-line-item/time-line-item.component.spec.ts
@@ -1,16 +1,20 @@
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import {BelAirColor} from '../../Interfaces';
+import { localStorageMock } from '../../testing/localStorage.mock';
 import {TimeLineItemComponent} from './time-line-item.component';
+import {TranslateTestingModule} from '../../testing/TranslateTestingModule';
+import {indexLabel, lightIndexColor} from '../../common/constants';
 
-xdescribe('TimeLineItemComponent', () => {
+describe('TimeLineItemComponent', () => {
   let component: TimeLineItemComponent;
 
   let fixture: ComponentFixture<TimeLineItemComponent>;
+  const belAqiResults = localStorageMock.getIndexScores(5, 5);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ TimeLineItemComponent ],
+      imports: [TranslateTestingModule],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
     .compileComponents();
@@ -19,10 +23,19 @@ xdescribe('TimeLineItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TimeLineItemComponent);
     component = fixture.componentInstance;
+    component.data = belAqiResults[5];
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show proper color and labels', () => {
+    const newBelaqiResult = belAqiResults[3];
+    component.data = newBelaqiResult;
+    fixture.detectChanges();
+    expect(component.getColor()).toEqual(lightIndexColor[newBelaqiResult.indexScore]);
+    expect(component.getLabel()).toContain(indexLabel[newBelaqiResult.indexScore]);
   });
 });
