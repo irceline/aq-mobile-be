df14d90afb464534a61b20de03cab3bb18904c71
Joshua Hulpiau <joshua@nebulae.be>
2020-05-28 11:41:34 +0200

    small fix on rating screen and success display

--- src/app/v2/components/success-display/success-display.component.spec.ts
+++ src/app/v2/components/success-display/success-display.component.spec.ts
@@ -2,6 +2,7 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SuccessDisplayComponent } from './success-display.component';
+import {RouterModule} from '@angular/router';
 
 describe('SuccessDisplayComponent', () => {
   let component: SuccessDisplayComponent;
@@ -11,6 +12,7 @@ describe('SuccessDisplayComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ SuccessDisplayComponent ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [RouterModule.forRoot([])]
     })
     .compileComponents();
   }));
