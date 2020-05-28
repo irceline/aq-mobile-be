b7b7c476e8de7a37a62f3e41b5d8a6c61191a3e9
Antonio Copic <acopic.ext@autoscout24.com>
2020-05-28 20:34:03 +0200

    LocationSortableComponent specs

--- src/app/v2/components/location-sortable/location-sortable.component.spec.ts
+++ src/app/v2/components/location-sortable/location-sortable.component.spec.ts
@@ -3,27 +3,76 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LocationSortableComponent } from './location-sortable.component';
 import {TranslateTestingModule} from '../../testing/TranslateTestingModule';
+import { localStorageMock } from '../../testing/localStorage.mock';
+import {UserLocation} from '../../Interfaces';
+import {AlertController, IonItem, IonLabel, IonReorderGroup} from '@ionic/angular';
+import {By} from '@angular/platform-browser';
 
 describe('LocationSortableComponent', () => {
   let component: LocationSortableComponent;
   let fixture: ComponentFixture<LocationSortableComponent>;
+  let locations: UserLocation[];
+  const alertSpy = jasmine.createSpyObj('AlertController', ['create', 'present']);
+  const alertElementSpy = jasmine.createSpyObj('HTMLIonAlertElement', ['present']);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LocationSortableComponent ],
+      declarations: [
+          LocationSortableComponent,
+          IonReorderGroup,
+          IonItem,
+          IonLabel,
+      ],
+      providers: [
+        {provide: AlertController, useValue: alertSpy}
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       imports: [TranslateTestingModule]
     })
     .compileComponents();
+    locations = JSON.parse(localStorageMock.getItem('belAir.userLocations'));
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LocationSortableComponent);
     component = fixture.componentInstance;
+    component.locations = locations;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show initial locations', () => {
+    const de = fixture.debugElement;
+    const locationLabels = de.queryAll(By.css('ion-label'));
+    locationLabels.map((label, index) => {
+      expect(label.nativeElement.innerHTML).toContain(locations[index].label);
+    });
+  });
+
+  it('should create alert on delete location click', () => {
+    const de = fixture.debugElement;
+    alertElementSpy.present.and.callFake(() => Promise.resolve());
+    alertSpy.create.and.callFake(() => alertElementSpy);
+    fixture.detectChanges();
+    component.deleteLocation(locations[0]).then(() => {
+      expect(alertSpy.create).toHaveBeenCalled();
+      expect(alertElementSpy.present).toHaveBeenCalled();
+    });
+  });
+
+  it('should emit new order of locations on reorder', () => {
+    const reorderEventDetail = {
+      detail: {
+        from: 3,
+        to: 1,
+        complete: () => jasmine.createSpy()
+      }
+    };
+    spyOn(component.locationUpdated, 'emit');
+    component.doReorder(reorderEventDetail);
+    expect(component.locationUpdated.emit).toHaveBeenCalled();
   });
 });
