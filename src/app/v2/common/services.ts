e3fec6922c1351fa24892dbdee047b6bc4716c6a
Jan Schulte <j.schulte@52north.org>
2020-06-29 22:48:16 +0200

    add current belaqi value to start screen

--- src/app/v2/common/services.ts
+++ src/app/v2/common/services.ts
@@ -2,5 +2,6 @@ export const realtimeWmsURL = 'https://geo.irceline.be/realtime/wms';
 export const rioifdmWmsURL = 'https://geo.irceline.be/rioifdm/wms';
 export const forecastWmsURL = 'https://geo.irceline.be/forecast/wms';
 export const annualWmsURL = 'https://geo.irceline.be/annual/wms';
+export const rioifdmBelaqiWmsURL = 'https://geo.irceline.be/rioifdm/belaqi/wms';
 
 export const realtimeWfsURL  = 'https://geo.irceline.be/realtime/wfs';
--- src/app/v2/services/value-provider/belaqi-index.service.ts
+++ src/app/v2/services/value-provider/belaqi-index.service.ts
@@ -5,10 +5,11 @@ import moment from 'moment';
 import { forkJoin, Observable, Observer } from 'rxjs';
 
 import { createCacheKey } from '../../common/caching';
-import { forecastWmsURL, rioifdmWmsURL } from '../../common/services';
+import { forecastWmsURL, rioifdmBelaqiWmsURL, rioifdmWmsURL } from '../../common/services';
 import { UserLocation } from '../../Interfaces';
 import { BelAqiIndexResult } from '../bel-aqi.service';
 import { ForecastDateService } from '../forecast-date.service';
+import { IrcelineSettingsService } from '../irceline-settings/irceline-settings.service';
 import { ValueProvider } from './value-provider';
 
 enum BelaqiIndexForecastLayer {
@@ -26,7 +27,8 @@ export class BelaqiIndexService extends ValueProvider {
   constructor(
     protected http: HttpClient,
     private cacheService: CacheService,
-    private forecastDateSrvc: ForecastDateService
+    private forecastDateSrvc: ForecastDateService,
+    private ircelineSettings: IrcelineSettingsService
   ) {
     super(http);
   }
@@ -36,6 +38,7 @@ export class BelaqiIndexService extends ValueProvider {
       this.createPast(location, moment().subtract(3, 'day')),
       this.createPast(location, moment().subtract(2, 'day')),
       this.createPast(location, moment().subtract(1, 'day')),
+      this.createCurrent(location),
       this.createForecast(location, BelaqiIndexForecastLayer.TODAY, moment()),
       this.createForecast(location, BelaqiIndexForecastLayer.TOMORROW, moment().add(1, 'day')),
       this.createForecast(location, BelaqiIndexForecastLayer.TWO_DAYS, moment().add(2, 'day')),
@@ -50,25 +53,27 @@ export class BelaqiIndexService extends ValueProvider {
         rioifdmWmsURL, { responseType: 'json', params: params });
       this.cacheService.loadFromObservable(createCacheKey(rioifdmWmsURL, JSON.stringify(params), day.format('YYYY-MM-DD')), request)
         .subscribe(
-          res => {
-            const idx = this.getValueOfResponse(res);
-            if (idx) {
-              observer.next({
-                indexScore: Math.round(idx),
-                date: day,
-                location: location
-              });
-            } else {
-              observer.next(null);
-            }
-            observer.complete();
-          },
-          error => {
-            console.error(error);
-            console.error(`Couldn't get belaqi index for '${day.fromNow()}'`);
-            observer.next(null);
-            observer.complete();
-          });
+          res => this.handleResponse(res, observer, day, location),
+          error => this.handleError(error, observer)
+        );
+    });
+  }
+
+  private createCurrent(location: UserLocation): Observable<BelAqiIndexResult> {
+    return new Observable<BelAqiIndexResult>((observer: Observer<BelAqiIndexResult>) => {
+      this.ircelineSettings.getSettings().subscribe(
+        settings => {
+          const params = this.createFeatureInfoRequestParams('rioifdm:belaqi', location, settings.lastupdate.toISOString());
+          const request = this.http.get<GeoJSON.FeatureCollection<GeoJSON.GeometryObject>>(
+            rioifdmBelaqiWmsURL, { responseType: 'json', params: params });
+          this.cacheService.loadFromObservable(createCacheKey(rioifdmBelaqiWmsURL,
+            JSON.stringify(params),
+            settings.lastupdate.toISOString()
+          ), request).subscribe(
+            res => this.handleResponse(res, observer, moment(settings.lastupdate.toISOString()), location),
+            error => this.handleError(error, observer)
+          );
+        });
     });
   }
 
@@ -81,28 +86,32 @@ export class BelaqiIndexService extends ValueProvider {
           const request = this.http.get<GeoJSON.FeatureCollection<GeoJSON.GeometryObject>>(url, { responseType: 'json', params: params });
           this.cacheService.loadFromObservable(createCacheKey(url, JSON.stringify(params), date.toISOString()), request)
             .subscribe(
-              res => {
-                const idx = this.getValueOfResponse(res);
-                if (idx) {
-                  observer.next({
-                    indexScore: Math.round(idx),
-                    date: day,
-                    location: location
-                  });
-                } else {
-                  observer.next(null);
-                }
-                observer.complete();
-              },
-              error => {
-                console.error(error);
-                console.error(`Couldn't get belaqi index for '${day.fromNow()}'`);
-                observer.next(null);
-                observer.complete();
-              });
+              res => this.handleResponse(res, observer, day, location),
+              error => this.handleError(error, observer)
+            );
         }
       );
     });
+  }
+
+  private handleResponse(res: any, observer: Observer<BelAqiIndexResult>, date: moment.Moment, location: UserLocation) {
+    const idx = this.getValueOfResponse(res);
+    if (idx) {
+      observer.next({
+        indexScore: Math.round(idx),
+        date,
+        location: location
+      });
+    } else {
+      observer.next(null);
+    }
+    observer.complete();
+  }
+
+  private handleError(error: any, observer: Observer<BelAqiIndexResult>) {
+    console.error(error);
+    observer.next(null);
+    observer.complete();
   }
 
 }
