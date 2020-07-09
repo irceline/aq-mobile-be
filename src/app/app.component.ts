c4e7e7648ace3e8df8c61a85fea9aec24e22c7a7
Krešo <kreso.galic8@gmail.com>
2020-07-09 13:29:51 +0200

    fix: system back button not working #224

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,73 +1,41 @@
 import { Component, QueryList, ViewChildren } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { IonRouterOutlet, ModalController, NavController, Platform } from '@ionic/angular';
-import { filter } from 'rxjs/operators';
+import {
+    IonRouterOutlet,
+    ModalController,
+    NavController,
+    Platform,
+} from '@ionic/angular';
 
 import { ErrorLoggingService } from './v2/services/error-logging.service';
 import { PouchDBInitializerService } from './v2/services/pouch-db-initializer/pouch-db-initializer.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html'
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
 })
 export class AppComponent {
+    @ViewChildren(IonRouterOutlet) routerOutlets: QueryList<IonRouterOutlet>;
 
-  @ViewChildren(IonRouterOutlet) routerOutlets: QueryList<IonRouterOutlet>;
+    constructor(
+        private platform: Platform,
+        private splashScreen: SplashScreen,
+        private statusBar: StatusBar,
+        public router: Router,
+        private pouchDbInit: PouchDBInitializerService
+    ) {
+        this.initializeApp();
+    }
 
-  private lastNavigation = ['tabs/start', 'tabs/start', 'tabs/start'];
-  private lastNavigationStep = 1;
-
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
-    public router: Router,
-    private modalCtrl: ModalController,
-    private pouchDbInit: PouchDBInitializerService,
-    private errorLoggingSrvc: ErrorLoggingService,
-    private navCtrl: NavController
-  ) {
-    this.initializeApp();
-    this.backButtonEvent();
-    this.storeLastNavigation();
-  }
-
-  backButtonEvent() {
-    this.platform.backButton.subscribe(() => {
-      this.routerOutlets.forEach(async (outlet: IonRouterOutlet) => {
-        const modal = await this.modalCtrl.getTop();
-        if (modal) {
-          modal.dismiss();
-        } else if (this.router.url === '/tabs/start') {
-          navigator['app'].exitApp();
-        } else {
-          this.router.navigateByUrl(this.lastNavigation[this.lastNavigationStep]);
-          this.lastNavigationStep -= 2;
-        }
-      });
-    });
-  }
-
-  public storeLastNavigation(): void {
-    this.router.events
-      .pipe(filter(event => event instanceof NavigationEnd))
-      .subscribe(({ urlAfterRedirects }: NavigationEnd) => {
-        if (this.lastNavigationStep < 1) {
-          this.lastNavigationStep++;
-        }
-        this.lastNavigation = ['tabs/start', this.lastNavigation[2], urlAfterRedirects];
-      });
-  }
-
-  initializeApp() {
-    this.platform.ready().then(() => {
-      // this.statusBar.styleDefault();
-      // this.errorLoggingSrvc.init();
-      this.statusBar.show();
-      this.splashScreen.hide();
-      this.pouchDbInit.init();
-    });
-  }
+    initializeApp() {
+        this.platform.ready().then(() => {
+            // this.statusBar.styleDefault();
+            // this.errorLoggingSrvc.init();
+            this.statusBar.show();
+            this.splashScreen.hide();
+            this.pouchDbInit.init();
+        });
+    }
 }
