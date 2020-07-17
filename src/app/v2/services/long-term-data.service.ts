ca2f1e14e4687fe567908983e4ddb5cec13673b9
opeeters <opeeters@gmail.com>
2020-07-17 13:29:08 +0200

    adjust for EU-limit change 2020 (PM2.5) and add WHO PM10

--- src/app/v2/services/long-term-data.service.ts
+++ src/app/v2/services/long-term-data.service.ts
@@ -86,6 +86,8 @@ export class LongTermDataService {
     switch (phenomenon) {
       case MainPhenomenon.NO2:
         return 40;
+      case MainPhenomenon.PM10:
+        return 20;
       case MainPhenomenon.PM25:
         return 10;
       default:
@@ -98,7 +100,7 @@ export class LongTermDataService {
       case MainPhenomenon.NO2:
         return 40;
       case MainPhenomenon.PM25:
-        return 25;
+        return 20;
       default:
         return null;
     }
