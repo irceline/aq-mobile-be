1d9649e0373b22ceb698d108a73bde51af9fb238
Jan Schulte <j.schulte@52north.org>
2020-11-17 16:05:25 +0100

    add console logs for debugging

--- src/app/v2/services/encryption/encryption.service.ts
+++ src/app/v2/services/encryption/encryption.service.ts
@@ -22,7 +22,10 @@ export class EncryptionService {
           .then(() => {
             this.firebase.activateFetched().then(() => {
               this.firebase.getValue(FIREBASE_REMOTE_CONFIG_KEY, '')
-                .then(key => this.key = CryptoJS.enc.Base64.parse(key))
+                .then(key => {
+                  console.log(`Fetched key from firebase: '${key}'`);
+                  return this.key = CryptoJS.enc.Base64.parse(key);
+                })
                 // TODO: error handling!
                 .catch((error: any) => console.error(error));
             });
@@ -34,6 +37,7 @@ export class EncryptionService {
 
   public encrypt(data: string): string {
     if (this.key) {
+      console.log(`Encode data ${JSON.stringify(data, null, 0)} with key '${this.key.toString(CryptoJS.enc.Base64)}'`);
       const iv = CryptoJS.enc.Utf8.parse(CryptoJS.lib.WordArray.random(8));
       const cyphertext_raw = CryptoJS.AES.encrypt(data, this.key, { iv: iv, mode: CryptoJS.mode.CBC });
       const hmac = CryptoJS.HmacSHA256(cyphertext_raw.toString(), this.key.toString(CryptoJS.enc.Base64));
