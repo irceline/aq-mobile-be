19685ec4f74a1689b015a676024d309f77cc081c
Ivan <ivan@nebulae.be>
2025-11-12 16:40:37 +0700

    sync pods for analytic

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  FEEDBACK_SERVICE_URL: 'https://geobelair.irceline.be/air/belair_report.php'
+  FEEDBACK_SERVICE_URL: 'https://api-production-065b.up.railway.app/belair_report.php'
 };
